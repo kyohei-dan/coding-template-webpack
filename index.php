52cb6498c160caf75e7193a3bc8e13176731b0eb
kyohei_dan <kyohey.dan@gmail.com>
2022-01-28 23:52:06 +0900

    [Add] テンプレートのclass名を変更

--- index.php
+++ index.php
@@ -15,24 +15,24 @@
     <!--=============== ▼MAIN ===============-->
     <main class="p-main">
       <section class="p-news">
-        <div class="p-news--inner">
-          <article class="p-news--article">
-            <h1 class="p-news--heading test">testtesttesttest</h1>
-            <ul class="p-news--list">
-              <li class="p-news--list-item">
-                <span class="p-news--icon"><i class="bx bx-loader bx-spin  bx-md"></i></span>
-                <span class="p-news--label">通常ラベル</span>
+        <div class="p-news__inner">
+          <article class="p-news__article">
+            <h1 class="p-news__heading test">testtesttesttest</h1>
+            <ul class="p-news__list">
+              <li class="p-news__list-item">
+                <span class="p-news__icon"><i class="bx bx-loader bx-spin  bx-md"></i></span>
+                <span class="p-news__label">通常ラベル</span>
               </li>
-              <li class="p-news--list-item">
-                <span class="p-news--icon"><i class="bx bxs-bell bx-tada bx-md"></i></span>
-                <span class="p-news--label__red">赤ラベル</span>
+              <li class="p-news__list-item">
+                <span class="p-news__icon"><i class="bx bxs-bell bx-tada bx-md"></i></span>
+                <span class="p-news__label--red">赤ラベル</span>
               </li>
             </ul>
             <figure>
               <img src="https://picsum.photos/seed/picsum/500/350" alt="の画像" class="p-news__thumbnail" width="" height="" loading="lazy" decoding="async" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;">
             </figure>
-            <p class="p-news--text">ニュース記事</p>
-            <a href="https://boxicons.com/usage" class="p-news--link" target="_blank" rel="noopener noreferrer">詳しくはこちら</a>
+            <p class="p-news__text">ニュース記事</p>
+            <a href="https://boxicons.com/usage" class="p-news__link" target="_blank" rel="noopener noreferrer">詳しくはこちら</a>
           </article><!-- /.p-news--article -->
         </div><!-- /.p-news--inner -->
       </section><!-- /.p-news -->
